a9bebf5692acbea2db0da48860fe3891edf5087a
8A <32315516+Irlanda3@users.noreply.github.com>
2023-01-26 22:21:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+// Obtener elements del DOM
+const menuBtn = document.querySelector(".menu-btn"); //para seleccionar selectors in de Document
+const menuBtnMobile = document.querySelector(".menu-btn-mobile");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+const backdrop = document.querySelector(".sidebar-backdrop");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+//inicializar on scroll animations
+//AOS.init();
+
+//Add menu click events
+menuBtn.addEventListener("click", sidebarOpen);
+menuBtnMobile.addEventListener("click", sidebarOpen);
+
+/*Open sidebar function*/
+
+function sidebarOpen() {
+  /*cambia posision de sidebar */
+  sidebar.style.right = "0";
+  //muestra backdrop
+  backdrop.style.display = "block";
+  //muestra backdrop atravez de una transition suavez
+  setTimeout(() => {
+    backdrop.style.opacity = "1";
+  }, 50);
+  //deshabilita el scroll en el body
+  document.body.classList.add("sidebar-open-body");
+}
+
+//click event para close button
+closeBtn.addEventListener("click", () => {
+  //cambiamos la position de el sidebar
+  sidebar.style.right = "-20em";
+  //esconde backdrop  con una transicion suave
+  backdrop.style.opacity = "0";
+  setTimeout(() => {
+    backdrop.style.display = "none";
+  }, 300);
+  // haz q el body sea scrollable otra vez
+  document.body.classList.remove("sidebar-open-body");
+});
+
+//Custom scroll event (changing the image
+//Get elements formt he DOM
+const text = document.querySelector(".roof-text");
+const roofImg = document.querySelector(".banner-img");
+
+//Add a scroll event tot he document
+window.addEventListener("scroll", () => {
+  //Cuando el texto del elemento este animado
+  if (text.classList.contains("aos-animate")) {
+    //muestra la segunda imagen del roof
+    roofImg.classList.add("roof-2");
+  } else {
+    // regresate a la primera imagen de roof
+    roofImg.classList.remove("roof-2");
+  }
+});
